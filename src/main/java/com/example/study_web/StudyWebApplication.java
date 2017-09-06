f16363459a628d1268feb0e45dccce067d1ad186
junwoo <>
2017-09-06 12:50:48 +0900

    initialize project

---
+++ src/main/java/com/example/study_web/StudyWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.study_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWebApplication.class, args);
+	}
+}
